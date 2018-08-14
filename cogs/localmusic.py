c7a0d3fcba4376653d0ff5d377645ab4dfa43c1e
sco1 <sco1.git@gmail.com>
2018-08-14 11:26:33 -0400

    Add check for users in voice channel
    Only stream music while someone is connected so we're not streaming 24/7

--- cogs/localmusic.py
+++ cogs/localmusic.py
@@ -14,14 +14,35 @@ class LocalMusic():
     async def on_ready(self):
         elevatorchannel = self.bot.get_channel(self.bot.elevatorchannelID)
         self.VC = await elevatorchannel.connect()
+        self._VCchannel = self.VC.channel
 
-        player = discord.FFmpegPCMAudio(self.musicpath)
-        self.VC.play(player)
+        self.player = discord.FFmpegPCMAudio(self.musicpath)
+        # Only start player if other users are in the voice channel
+        if len(self._VCchannel.members) > 1:
+            self.play(self.player)
 
-    @commands.command()
-    async def play(self, ctx: commands.Context):
-        player = self.bot.VC.create
-        raise NotImplementedError
+        # TODO: Generate embed loop
+
+    def play(self):
+        if not self.VC.is_playing():
+            if not self.VC.is_paused():
+                self.VC.play(self.player)
+            else:
+                self.VC.resume()
+
+    async def on_voice_state_update(self, member, before, after):
+        if member == self.bot.user:
+            # Ignore bot events
+            return
+        
+        if before.channel != self._VCchannel and after.channel == self._VCchannel:
+            # User joined channel, start/resume music
+            # LocalMusic.start() makes the paused vs. not playing check
+            self.play()
+        elif before.channel == self._VCchannel and after.channel != self._VCchannel:
+            # User left channel, pause if they were the last one
+            if len(self._VCchannel.members) <= 1:
+                self.VC.pause()
 
 def setup(bot):
     bot.add_cog(LocalMusic(bot))
